b2b21c532193e1f418f9236e59b93e41e33d134c
Sakshi Sawant <sakshisawant597@gmail.com>
2026-08-06 02:00:07 +0530

    Load environment variables using python-dotenv

--- core/core/settings.py
+++ core/core/settings.py
@@ -1,4 +1,12 @@
+
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
 """
 Django settings for core project.
 
